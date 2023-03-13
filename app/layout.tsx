136446114f4fd98722ecab8927bcac5029f7b484
Sarit <vertuser@VertLtd.local>
2023-03-13 14:06:48 +0300

    Server component error rectification

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-export const metadata = {
-  title: 'Astra Technologies ',
-  description: 'Astra Technologies is your IT Tech company for your solutions.',
-}
-
 export default function RootLayout({
   children,
 }: {
